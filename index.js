f3c3c43fe15bd0fafda7d9f9aec62d46d6f747aa
ThePopcornNoob <tomdeansegev@gmail.com>
2018-09-23 15:44:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,41 +3,59 @@ const bot = new commando.Client();
 
 
 bot.on('message', function(message){    
-    if(message.content == '^^join Hebrew')
+    if(message.content == '^^join DPS')
     {
-        message.channel.send('Added ' + message.author + ' to Hebrew role');
-        let memberRole = message.member.guild.roles.find("name", "Hebrew");
+        message.channel.send('Added ' + message.author + ' to DPS role');
+        let memberRole = message.member.guild.roles.find("name", "DPS");
         message.member.addRole(memberRole);
     }
 });
 
 bot.on('message', function(message){    
-    if(message.content == '^^join hebrew')
+    if(message.content == '^^join dps')
     {
-        message.channel.send('Added ' + message.author + ' to Hebrew role');
-        let memberRole = message.member.guild.roles.find("name", "Hebrew");
+        message.channel.send('Added ' + message.author + ' to DPS role');
+        let memberRole = message.member.guild.roles.find("name", "DPS");
         message.member.addRole(memberRole);
     }
 });
 
 bot.on('message', function(message){    
-    if(message.content == '^^join English')
+    if(message.content == '^^join Flex')
     {
-        message.channel.send('Added ' + message.author + ' to English role');
-        let memberRole = message.member.guild.roles.find("name", "English");
+        message.channel.send('Added ' + message.author + ' to Flex role');
+        let memberRole = message.member.guild.roles.find("name", "Flex");
         message.member.addRole(memberRole);
     }
 });
 
 
 bot.on('message', function(message){    
-    if(message.content == '^^join english')
+    if(message.content == '^^join flex')
     {
-        message.channel.send('Added ' + message.author + ' to English role');
-        let memberRole = message.member.guild.roles.find("name", "English");
+        message.channel.send('Added ' + message.author + ' to Flex role');
+        let memberRole = message.member.guild.roles.find("name", "Flex");
         message.member.addRole(memberRole);
     }
 });
+
+bot.on('message', function(message){    
+    if(message.content == '^^join Support')
+    {
+        message.channel.send('Added ' + message.author + ' to Support role');
+        let memberRole = message.member.guild.roles.find("name", "Support");
+        message.member.addRole(memberRole);
+    }
+});
+
+bot.on('message', function(message){    
+    if(message.content == '^^join support')
+    {
+        message.channel.send('Added ' + message.author + ' to Support role');
+        let memberRole = message.member.guild.roles.find("name", "Support");
+        message.member.addRole(memberRole);
+    }
+
 
 bot.on('ready',function(){
     console.log("Ready");
